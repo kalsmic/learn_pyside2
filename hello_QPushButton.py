913fbaea25ef03646af8ddcd4108dde72b886c74
Arthur Kalule <kalulearthur@gmail.com>
2021-01-31 16:20:28 +0300

    add QPushButton event

--- hello_QPushButton.py
+++ hello_QPushButton.py
@@ -1,9 +1,32 @@
-from PySide2.QtWidgets import QApplication, QPushButton
+from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
+
+from PySide2.QtCore import Qt
+
+import sys
+
+#  subclass the mainwindow to customize my applications main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+        # set the central widget of the window
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+
 
 #  create QApplication instance
-app = QApplication([])
+app = QApplication(sys.argv)
 
-window = QPushButton("Push Me")
+window = MainWindow()
 window.show() # windows are hidden by default
 
 #  start the event loop
